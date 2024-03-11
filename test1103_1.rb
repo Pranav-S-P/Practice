de40727bbcfb2e9fe95c160e4465ec755cbaabc6
Pranav-S-P <pranavsuresh.priya@yubisecurities.com>
2024-03-11 18:26:15 +0530

    Third Commit to Change from regular to OOPs

--- test1103_1.rb
+++ test1103_1.rb
@@ -1,24 +1,32 @@
-l=[]
-def pushes(l,a)
-    l.append(a)
-end
+class Stackss
+    attr_accessor :lis
 
-def popes(l)
-    if l.length==0
-        puts "\nNo element in the stack"
-    else
-        l.delete(l[l.length-1])
+    def initialize
+        @lis=[]
+    end
+
+    def pushes(a)
+        @lis.append(a)
+    end
+
+    def popes()
+        if @lis.length==0
+            puts "\nNo element in the stack"
+        else
+            @lis.pop
+        end
+    end
+
+    def topes()
+        if @lis.length==0
+            puts "\nNo element in the stack"
+        else
+            puts "\nThe top of stack is #{@lis[@lis.length-1]}"
+        end
     end
 end
 
-def topes(l)
-    if l.length==0
-        puts "\nNo element in the stack"
-    else
-        puts "\nThe top of stack is #{l[l.length-1]}"
-    end
-end
-
+l=Stackss.new
 while true
     puts "What operation would u want to perform\n\n1.Push\n2.Pop\n3.Peak\n4.Quit\n\n Enter your Choice Number : "
     b=gets.chomp()
@@ -26,11 +34,11 @@ while true
     when '1'
         puts "\nEnter the element to be Pushed into the stack: "
         inp=gets.chomp()
-        pushes(l,inp)
+        l.pushes(inp)
     when '2'
-        popes(l)
+        l.popes()
     when '3'
-        topes(l)
+        l.topes()
     when '4'
         puts "Quitting..."
         break
